18dc1c19716556c340e461ce6c2b7f23fd0e6883
Filip Kotenko <39651273+Kotfil@users.noreply.github.com>
2019-10-17 23:59:09 +0300

    new2

--- Build/src/index.js
+++ Build/src/index.js
@@ -1,36 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const TodoList = () => {
+// import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list';
 
-const items = ['Drink Cofee', 'Build Awesome App'];
-return (
-<ul>
-        <li>{items[0]} </li>
-        <li>{items[1]} </li>
-        </ul>
 
-    );
-};
-const AppHeader = () => {
-    return <h1>My Todo List</h1>;
-};
+        const AppHeader = () => {
+            return <h1> My Todo List </h1>;
+        };
 
-const SearchPanel = () => {
-    return <input placeholder = "search" />;
-    };
+
 
 const App = () => {
 
     return (
-    <div>
-        <span> { (new Date()).toString()}</span>
+   <div>
         <AppHeader />
         <SearchPanel />
         <TodoList />
-</div>
+    </div>
     );
 };
 
- ReactDOM.render(< App />,
-  document.getElementById('root'));
+ ReactDOM.render(<App />,
+ document.getElementById('root'));
