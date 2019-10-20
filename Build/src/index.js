f0336b218724b70cf0d7f1a524f2dffdf5e80737
Filip Kotenko <39651273+Kotfil@users.noreply.github.com>
2019-10-20 03:22:55 +0300

    new4

--- Build/src/index.js
+++ Build/src/index.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// import AppHeader from './components/app-header';
+import AppHeader from './components/app-header';
 import SearchPanel from './components/search-panel';
 import TodoList from './components/todo-list';
 
-
-        const AppHeader = () => {
-            return <h1> My Todo List </h1>;
-        };
-
-
-
 const App = () => {
-
+    
+    const todoData = [
+        {label: 'Drink Coffe', important: false, id: 1 },
+        {label: 'Make Awesome App', important: true, id: 2 },
+        {label: 'Have a lunch', important:false, id: 3 }
+    ];
+    
     return (
    <div>
         <AppHeader />
         <SearchPanel />
-        <TodoList />
+        <TodoList todos={todoData} />
     </div>
     );
 };
