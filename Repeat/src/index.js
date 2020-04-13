cdd92351f3679e39b507add719e877b5041f37e9
FIlipp <filippkotenko@outlook.com>
2020-04-13 18:13:59 +0300

    36 state

--- Repeat/src/index.js
+++ Repeat/src/index.js
@@ -1,25 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppHeader from "./components/app-header";
-import SearchPanel from "./components/search-panel";
-import TodoList from "./components/todo-list";
+import App from './components/app/app';
+import'./index.css';
 
-const App = () => {
 
-    const todoData = [
-        {label: 'Drink Coffe', important: false,id:1},
-        {label: 'Make Awesome App', important: true,id:2},
-        {label: 'Have a lunch', important: false,id:3}
-    ]
-    return (
-        <div>
-            <TodoList todos={todoData}/>
-            <AppHeader/>
-            <SearchPanel/>
 
-        </div>
-    );
-}
 
-ReactDOM.render(<App/>,
-    document.getElementById('root'));
+
+ReactDOM.render(<App />,
+  document.getElementById('root'));
+
