edf1e5f7fb32aede315766536e768ba7ff2ef9cb
FIlipp <filippkotenko@outlook.com>
2020-04-28 00:27:34 +0300

    state Tasks

--- Repeat3/src/TodoListTask.js
+++ Repeat3/src/TodoListTask.js
@@ -2,16 +2,17 @@ import React from 'react';
 import './App.css';
 
 
-class TodoListTasks extends React.Component {
+class TodoListTask extends React.Component {
     render = () => {
         return (
             <div className="TodoList-task">
                 <input type="checkbox" checked={this.props.isDone}/>
                 <span>{this.props.title}</span>
+                <div>priority: {this.props.priority}</div>
             </div>
         );
     }
 }
 
-export default TodoListTasks;
+export default TodoListTask;
 
